9eb6024233264e4b001142f5b0ef080c09566ddd
Edward Cragg <edward.cragg@codethink.co.uk>
2016-05-27 12:49:16 +0100

    Fix DHCP setup script

--- install-dhcp.sh
+++ install-dhcp.sh
@@ -4,14 +4,20 @@
 
 sudo apt-get install dnsmasq
 sudo systemctl stop dnsmasq
+
 sudo cp /etc/dnsmasq.conf /etc/dnsmasq.conf-backup
 
-sudo tee /etc/dnsmasq.conf < EOF
+sudo tee /etc/dnsmasq.conf << EOF
 interface=$IF_INT
 dhcp-range=$DHCP_START,$DHCP_END,72h
+# Enable the TFTP server
+enable-tftp
+tftp-root=/srv/tftp
 EOF
 
-sudo systemctl start dnsmasq
+sudo mkdir -p /srv/tftp
+
+sudo systemctl start dnsmasq.service
 
 #---
 # REF: https://help.ubuntu.com/community/Internet/ConnectionSharing
