5772e5edafa80adc88db9be5d8a700b190f18441
Edward Cragg <edward.cragg@codethink.co.uk>
2016-05-27 14:39:58 +0100

    Fix nfs install script

--- install-nfs-tftp.sh
+++ install-nfs-tftp.sh
@@ -8,6 +8,8 @@
 echo "Installing NFS server"
 sudo apt-get -y install nfs-kernel-server
 
+sudo systemctl stop nfs-kernel-server.service
+
 echo "Configuring NFS server"
 echo "Creating rootfs share dir"
 sudo mkdir -p /srv/rootfs
@@ -16,4 +18,5 @@ sudo tee -a /etc/exports << EOF
 /srv/rootfs  $SN_INT(rw,no_root_squash,no_subtree_check)
 EOF
 
-sudo systemctl start 
+echo "Starting services"
+sudo systemctl start nfs-kernel-server.service
